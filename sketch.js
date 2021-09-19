9522b3b3757aab190364f3c51dde8be8be52e7d9
bhavyaben-prajapati <90895984+bhavyaben-prajapati@users.noreply.github.com>
2021-09-19 13:17:55 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
-
+var ball
 function setup() {
   createCanvas(400,400);
+  ball = createSprite(200, 200, 50, 50
 }
 
 function draw() 
 {
-  background(30);
-
+  background("pink");
+  drawSprites ()
 }
 
 
